f9f148dde54410ae028a91ee4be73d8b248e6708
Saurabh Ajmera <sajmera@nfml-sajmeranrf.corp.netflix.com>
2017-01-17 09:40:41 -0800

    working version of auth0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[AuthService],
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private auth:AuthService){}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HttpModule, Http, RequestOptions } from '@angular/http';
+import { provideAuth, AuthHttp, AuthConfig } from 'angular2-jwt';
+
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp( new AuthConfig({}), http, options);
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [ Http, RequestOptions ]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
